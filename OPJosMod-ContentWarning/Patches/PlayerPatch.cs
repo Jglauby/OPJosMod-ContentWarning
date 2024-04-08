340dd410ba1c15bfe7416b86d83f0d91272fdc03
Jglauby <jglauby@gmail.com>
2024-04-08 03:28:18 -0500

    reworked how it works, now prevents death but makes you ragdoll for a period of time then auto revives you

--- OPJosMod-ContentWarning/Patches/PlayerPatch.cs
+++ OPJosMod-ContentWarning/Patches/PlayerPatch.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx.Logging;
 using HarmonyLib;
+using System.Reflection;
 using UnityEngine;
 
 namespace OPJosMod_ContentWarning.SelfRevive.Patches
@@ -13,23 +14,74 @@ namespace OPJosMod_ContentWarning.SelfRevive.Patches
             mls = logSource;
         }
 
+        public static bool AutoRevive = true;
+        public static PlayerRagdoll localPLayer;
+        public static MethodInfo ragdollMethod;
+        public static bool isRagdoll;
+        public static float lastCalled = Time.time;
+        public static float timeDied = Time.time;
+
         [HarmonyPatch("Update")]
         [HarmonyPostfix]
         private static void patchUpdate(Player __instance)
         {
             if (__instance.IsLocal && Input.GetKeyDown(KeyCode.K))
+                __instance.Die();
+
+            if (__instance.IsLocal && Input.GetKeyDown(KeyCode.L))
             {
-                if (__instance.data.dead)
+                if (AutoRevive)
                 {
-                    mls.LogMessage($"revived {__instance.name}");
-                    __instance.CallRevive();
+                    mls.LogMessage($"auto revive off");
+                    AutoRevive = false;
+
+                    if (isRagdoll)
+                        __instance.Die();
                 }
                 else
                 {
-                    mls.LogMessage($"killed {__instance.name}");
-                    __instance.Die();
+                    mls.LogMessage($"auto revive on");
+                    AutoRevive = true;
                 }
             }
+
+            if (isRagdoll && Time.time - timeDied > 3f)
+            {
+                isRagdoll = false;
+            }
+            else if (isRagdoll && Time.time - lastCalled > 0.25f)
+            {              
+                lastCalled = Time.time;
+                ragdollMethod.Invoke(localPLayer, new object[1] { 1f });
+            }         
+        }
+
+        [HarmonyPatch("Awake")]
+        [HarmonyPostfix]
+        private static void patchAwake(Player __instance)
+        {
+            if (__instance.IsLocal)
+            {
+                localPLayer = ((Component)__instance).gameObject.GetComponent<PlayerRagdoll>();
+                ragdollMethod = ((object)localPLayer).GetType().GetMethod("CallFall", BindingFlags.Instance | BindingFlags.NonPublic);
+            }
+        }
+
+        [HarmonyPatch("Die")]
+        [HarmonyPrefix]
+        private static bool patchDie(Player __instance)
+        {
+            if (AutoRevive && __instance.IsLocal)
+            {
+                isRagdoll = true;
+                lastCalled = Time.time;
+                timeDied = Time.time;
+                __instance.data.health = 10;
+
+                return false;
+            }
+
+            return true;
         }
     }
 }
