79bfe69d90aa14e330c06b95b6993e601b682a07
Jglauby <jglauby@gmail.com>
2024-04-10 03:40:00 -0500

    added basic config setup to master

---
+++ OPJosMod-ContentWarning/ConfigVariables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace OPJosMod_ContentWarning
+{
+    public static class ConfigVariables
+    {
+        public static KeyCode ReviveButton;
+        public static KeyCode AutoReviveButton;
+    }
+}
--- OPJosMod-ContentWarning/Plugin.cs
+++ OPJosMod-ContentWarning/Plugin.cs
@@ -2,6 +2,7 @@
 using BepInEx.Logging;
 using HarmonyLib;
 using OPJosMod_ContentWarning.ExampleMod.Patches;
+using UnityEngine;
 
 namespace OPJosMod_ContentWarning.ExampleMod
 {
@@ -25,10 +26,30 @@ namespace OPJosMod_ContentWarning.ExampleMod
             }
             mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
             mls.LogInfo("mod has started!");
+            setupConfig();
 
             PlayerPatch.SetLogSource(mls);
 
             harmony.PatchAll();
+        }
+
+        private void setupConfig()
+        {
+            var configReviveButton = Config.Bind("Revive / KYS Button",
+                                        "ReviveButton",
+                                        KeyCode.K,
+                                        "Toggles between KYS-ing and reviving");
+
+            var configAutoReviveButton = Config.Bind("Auto Revive Toggle",
+                                        "AutoReviveButton",
+                                        KeyCode.L,
+                                        "Toggles on and off auto revive");
+
+
+            ConfigVariables.ReviveButton = configReviveButton.Value;
+            ConfigVariables.AutoReviveButton = configAutoReviveButton.Value;
+
+            Config.Save();
         }
     }
 }
